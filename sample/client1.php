73f633df932dcfebc8427453066dc9a2719f8df3
Jay Zhang <jay@easilydo.com>
2012-09-28 17:19:26 +0800

    fix sample client1.php typo bug

--- sample/client1.php
+++ sample/client1.php
@@ -2,8 +2,8 @@
 
 require(dirname(__FILE__)."/../library/Jsonrpc5/include.php");
 
-$protocol = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "http" : "https";
-$url = $protocol."://".$_SEVER["HTTP_HOST"].dirname($_SERVER["REQUEST_URI"])."/server1.php";
+$protocol = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? "https" : "http";
+$url = $protocol."://".$_SERVER["HTTP_HOST"].dirname($_SERVER["REQUEST_URI"])."/server1.php";
 $client = new Jsonrpc5_Client($url);
 var_dump($client->add(2,3)); // output 5
 var_dump($client->mult(2,3)); // output 6
